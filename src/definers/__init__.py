ff3e0b30cd819e9fea1d29e9848bdbb87b2371ce
Yaron Koresh <aharonkoresh1@gmail.com>
2025-09-20 15:36:38 +0300

    Update __init__.py

--- src/definers/__init__.py
+++ src/definers/__init__.py
@@ -977,8 +977,8 @@ def answer(history: list):
 
     generate_ids = MODELS["answer"].generate(
         **inputs,
-        max_new_tokens=512,
-        num_beams=8,
+        max_new_tokens=1024,
+        num_beams=6,
         length_penalty=0.1,
         num_logits_to_keep=1,
     )
