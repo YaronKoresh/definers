a9dda6f4140b8a2be9d780fbf606eaf52333977f
Yaron Koresh <aharonkoresh1@gmail.com>
2025-09-26 10:17:22 +0300

    Update __init__.py

--- src/definers/__init__.py
+++ src/definers/__init__.py
@@ -5667,7 +5667,7 @@ def pipe(
         params2["prompt"] = prompt
         params2["height"] = height
         params2["width"] = width
-        params2["guidance_scale"] =9.0
+        params2["guidance_scale"] =8.0
         if task == "video":
             params2["num_videos_per_prompt"] = 1
             params2["num_frames"] = length
