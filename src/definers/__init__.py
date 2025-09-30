a84ae5ad2cdaf749e585465572e90211e50e31a8
Yaron Koresh <aharonkoresh1@gmail.com>
2025-09-30 05:21:09 +0300

    Update __init__.py

--- src/definers/__init__.py
+++ src/definers/__init__.py
@@ -10366,8 +10366,8 @@ def start(proj: str):
     global np
     global _np
     
-    pip_install("numpy==1.26.4 scipy==1.12.0 transformers==4.53.0 nltk==3.8.1")
-
+    pip_install("accelerate==1.10.1 bitsandbytes==0.47.0 torch==2.8.0 numpy==1.26.4 scipy==1.12.0 transformers==4.53.0 nltk==3.9.1")
+    
     np, _np = patch_cupy_numpy()
     
     proj = proj.strip().lower()
