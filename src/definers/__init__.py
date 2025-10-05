de3ad4a7d0aa61e3d1e42338d8e36b20e8659390
Yaron Koresh <aharonkoresh1@gmail.com>
2025-10-05 19:07:21 +0300

    Update __init__.py

--- src/definers/__init__.py
+++ src/definers/__init__.py
@@ -1229,7 +1229,7 @@ def answer(history: list):
     generate_ids = MODELS["answer"].generate(
         **inputs,
         **stochastic_kwargs,
-        max_length=16384,
+        max_length=1024,
         num_logits_to_keep=1,
     )
 
