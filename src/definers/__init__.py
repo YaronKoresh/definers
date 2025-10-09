3a116a3fa505faffc8b7d2c035affd8afaa955e9
Yaron Koresh <aharonkoresh1@gmail.com>
2025-10-09 12:37:42 +0300

    Modify beam_kwargs for model optimization
    Adjusted beam search parameters for model tuning.

--- src/definers/__init__.py
+++ src/definers/__init__.py
@@ -58,13 +58,13 @@ SYSTEM_MESSAGE = None
 
 beam_kwargs = {
     "do_sample": False,
-    "top_k": 8,
+    "top_k": 4,
     "temperature": 0.9,
     "no_repeat_ngram_size": 3,
     "num_beams": 8,
     "early_stopping": True,
-    "length_penalty": -0.1,
-    "repetition_penalty": 1.1,
+    "length_penalty": -0.3,
+    "repetition_penalty": 1.3,
 }
 
 ai_model_extensions = [
